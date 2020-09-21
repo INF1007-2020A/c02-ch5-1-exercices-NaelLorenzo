4c9543b366f41a0753f80bc7e7f35cf3a4414e9b
Lorenzo Florenty <lorenzo.florenty@gmail.com>
2020-09-21 13:19:22 -0400

    Rectification

--- exercice.py
+++ exercice.py
@@ -3,25 +3,27 @@
 
 import random
 
-def get_bill(name, data):
-	INDEX_NAME = 0
-	INDEX_QUANTITY = 1
-	INDEX_PRICE = 2
 
-	return ""
+def get_bill(name, data):
+    INDEX_NAME = 0
+    INDEX_QUANTITY = 1
+    INDEX_PRICE = 2
+
+    return ""
 
 
 def format_number(number, num_decimal_digits):
-	return ""
+    return ""
+
 
 def get_triangle(num_rows):
-	return ""
+    return ""
 
 
 if __name__ == "__main__":
-	print(get_bill("Äpik Gämmör", [("chaise", 1, 399.99), ("g-fuel", 69, 35.99)]))
+    print(get_bill("Äpik Gämmör", [("chaise", 1, 399.99), ("g-fuel", 69, 35.99)]))
 
-	print(format_number(-12345.678, 2))
+    print(format_number(-12345.678, 2))
 
-	print(get_triangle(2))
-	print(get_triangle(5))
+    print(get_triangle(2))
+    print(get_triangle(5))
